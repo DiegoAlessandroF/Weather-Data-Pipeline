638ea78b2637ebffbcc4e6f8df507dc5ce9e48ec
Diego <diegodataeng@outlook.com>
2026-04-08 21:33:39 +0000

    chore: atualiza requirements.txt com airflow e dbt

---
+++ airflow/dags/weather-pipeline.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'diego',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='weather_pipeline',
+    default_args=default_args,
+    description='Pipeline meteorológico – coleta, carrega e transforma',
+    schedule_interval='0 6 * * *'   #todo dia às 6h UTC
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=['weather', 'pipeline'],
+) as dag:
+
+    coleta = BashOperator(
+        task_id='coleta_dados',
+        bash_command='cd ~/weather-pipeline && source .venv/bin/activate && python -m ingestion.collect',
+    )
+
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command='cd ~/weather-pipeline/dbt && source ~/.venv/bin/activate && dbt run',
+    )
+
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command='cd ~/weather-pipeline/dbt && source ~/.venv/bin/activate && dbt test',
+    )
+
+    coleta >> dbt_run >> dbt_test
